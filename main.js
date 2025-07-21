b863e5701a5214ea650e0a93379263fb1a6045eb
Amonrat <109088709+guzzle999@users.noreply.github.com>
2025-07-21 19:03:15 +0700

    fix: move function

--- main.js
+++ main.js
@@ -20,10 +20,11 @@ class Field {
 
 
 	print() {
-		console.log(this.field.map(row => row.join("░")).join("\n"));
+		console.log(this.field.map(row => row.join(" ")).join("\n"));
 	    }
 
-	static createField(row,col,holeCount=2) {
+	//field
+	static createField(row,col,holeCount=3) {
 		const field = Array.from({length: row}, ()=>Array.from({length: col}, ()=>"░"));
 
 	//holes
@@ -31,63 +32,69 @@ class Field {
 	while (i < holeCount) {
 		const holesposX = Math.floor(Math.random()*col);
 		const holesposY = Math.floor(Math.random()*row);
-	 	if (field[holesposY][holesposX] ==="░" && !(holesposX=== 0 && holesposY === 0)) {
-		field[holesposY][holesposX] = "O";
+	 	if (field[holesposY][holesposX] === "░" && !(holesposX === 0 && holesposY === 0)) {
+			field[holesposY][holesposX] = "O";
 		i++;
 	 }
 	}
 
 	//hat
-	const hatposX = Math.floor(Math.random()*col);
-	const hatposY = Math.floor(Math.random()*row);
-	field[hatposY][hatposX] = "^"
-
+	let hatPosition = false;
+	while (!hatPosition) {
+		const hatposX = Math.floor(Math.random()*col);
+		const hatposY = Math.floor(Math.random()*row);
+		if (field[hatposY][hatposX] === "░" && !(hatposX=== 0 && hatposY === 0)) {
+			field[hatposY][hatposX] = "^";
+			hatPosition = true;
+		}
+	}
 	//actor
 	field[0][0] = "*"
 	return field;
 	}
 
 	//Move Fucntion
-
 	move(direction) {
 	let {x,y} = this.actorPosition;
 		switch (direction) {
-			case'8' : x -= 1;
+			case'8' : y -= 1;
 			break;
-			case'5' : x += 1;
+			case'5' : y += 1;
 			break;
-			case'4' : y -= 1;
+			case'4' : x -= 1;
 			break;
-			case'6' : y += 1;
+			case'6' : x += 1;
+			break;
 			default:
 				console.log("Please input number to move, use '4' to moveLeft, '6' to moveRight, '8' to moveUp, and '5' to moveDown");
 				return;
 		}
 
 		if ( y < 0 || y>= this.field.length || x < 0 || x >= this.field[0].length) {
-			console.log('Game Over');
+			console.log("You walked off the field. Game Over!");
 			this.gameOver = true;
 			return;
 		}
 
-		const way = this.field[y][x];
+		const nextStep = this.field[y][x];
 
-		if (way === "0") {
+		if (nextStep === "O") {
 			console.log("You fell in to a hole. Game Over!");
 			this.gameOver = true;
-		} else if (way === "^") {
+		} else if (nextStep === "^") {
 			console.log("Yeah! You found your hat! You win!");
 			this.gameOver = true;
 		} else {
-			this.actorPosition = {x, y} ;
-			this.field [x][y] = "*";
+			this.field[this.actorPosition.y][this.actorPosition.x] = pathCharacter;
+			this.actorPosition = {x, y};
+			this.field[this.actorPosition.y][this.actorPosition.x] = pathCharacter;
 		}
 	}
 
 }
 
 function startGame() {
-	const fieldData = Field.createField(3,3);
+	const fieldData = Field.createField(4,4);
 	const newField = new Field(fieldData);
 
 	while (!newField.gameOver) {
